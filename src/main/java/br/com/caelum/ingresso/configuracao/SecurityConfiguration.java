e6e62f83a6c2e8fd68bb23f494929b37359340e4
mcesarmbarbosa <mcesarmbarbosa@gmail.com>
2017-08-12 17:29:26 -0300

    atualizacoes parte tarde

--- src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
+++ src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
@@ -1,13 +1,20 @@
 package br.com.caelum.ingresso.configuracao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @EnableWebSecurity
+@ImportResource("/WEB-INF/spring-context.xml")
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private UserDetailsService userDetailService;
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
