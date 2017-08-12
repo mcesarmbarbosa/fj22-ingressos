9712a3b12b8feead4166fd588024dacfba1568d1
mcesarmbarbosa <mcesarmbarbosa@gmail.com>
2017-08-12 17:13:06 -0300

    atualizacoes parte tarde

---
+++ src/main/java/br/com/caelum/ingresso/configuracao/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package br.com.caelum.ingresso.configuracao;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		    .csrf().disable().authorizeRequests()
+		        .antMatchers("/admin/**").hasRole("ADMIN")
+		        .antMatchers("/compra/**").hasRole("COMPRADOR")
+		        .antMatchers("/filme/**").permitAll()
+		        .antMatchers("/sessao/**/lugares").permitAll()
+		        .antMatchers("/magic/**").permitAll()
+		        .antMatchers("/").permitAll()
+		    .anyRequest()
+		        .authenticated()
+		    .and()
+		        .formLogin()
+		           .usernameParameter("email")
+		           .loginPage("/login")
+		           .permitAll();
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/assets/**");
+	}
+}
